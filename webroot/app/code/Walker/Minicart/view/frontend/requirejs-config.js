bba997415cc2e5361b75773d0f27bbfbbed2da99
Chris Walker <cwalker@trellis.co>
2020-05-04 21:56:14 -0500

    Minicart mixin

---
+++ webroot/app/code/Walker/Minicart/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            "Magento_Checkout/js/view/minicart": {
+                "Walker_Minicart/js/view/minicart-mixin": true
+            }
+        }
+    }
+};
