f2806cb4b30cb9ed36d9322c4651a2ab57da88a0
Chris Walker <cwalker@trellis.co>
2020-05-04 22:00:34 -0500

    A few examples

--- webroot/app/code/Mage2tv/Js/view/frontend/web/js/ui-component.js
+++ webroot/app/code/Mage2tv/Js/view/frontend/web/js/ui-component.js
@@ -1,11 +1,24 @@
-define(['uiCollection'], function (Collection) {
+define(['uiComponent'], function (Component) {
     "use strict";
 
-    return Collection.extend({
+    return Component.extend({
         defaults: {
-            template: 'Mage2tv_Js/ui-template',
             label: 'Some random numbers',
-            values: [22, 1, 5, 1024, 777]
+            additional_charge: 2,
+            items: [
+                {name: 'Surprise Box', qty: 5, price: 1.5},
+                {name: 'Chunk ', qty: 5, price: 1.5}
+            ],
+            tracks: {
+                additional_charge: true,
+                items: true
+            }
+        },
+        rowTotal: item => item.qty * item.price,
+        total: function () {
+            const sum = this.items.map(this.rowTotal)
+                .reduce((acc, total) => acc + total);
+            return sum + this.additional_charge;
         }
     });
 });
