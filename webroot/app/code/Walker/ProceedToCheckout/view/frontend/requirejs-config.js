013b07286ae6e3cfab211d7a56e7aa3bdfc8c2ae
Chris Walker <cwalker@trellis.co>
2020-05-22 14:12:14 -0500

    Proceed to checkout mixin

---
+++ webroot/app/code/Walker/ProceedToCheckout/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            "Magento_Checkout/js/proceed-to-checkout": {
+                "Walker_ProceedToCheckout/js/proceed-to-checkout-mixin": true
+            }
+        }
+    }
+}
