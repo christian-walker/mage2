cbfb537084b9e44acad503de7cc8290bb6d36bf4
Chris Walker <cwalker@trellis.co>
2020-05-25 16:25:02 -0500

    Added Minicart Mixin

--- webroot/app/code/Walker/Minicart/view/frontend/web/js/view/minicart-mixin.js
+++ webroot/app/code/Walker/Minicart/view/frontend/web/js/view/minicart-mixin.js
@@ -1,13 +1,20 @@
-define([], function () {
+define(['mage/utils/wrapper'], function (wrapper) {
     "use strict";
 
-    return function (Minicart) {
-        return Minicart.extend({
-            update: function (updatedCart) {
-                console.log('Update minicart successfully intercepted');
-                console.log(updatedCart);
-                return this._super(updatedCart);
-            }
+    return function (initSidebarFunction) {
+        return wrapper.wrap(initSidebarFunction, function (initSidebar, config, element) {
+            initSidebar(config, element);
+            console.log(this.element);
+
         });
+
+
+        // return Minicart.extend({
+        //     update: function (updatedCart) {
+        //         console.log('Update minicart successfully intercepted');
+        //         console.log(updatedCart);
+        //         return this._super(updatedCart);
+        //     }
+        // });
     }
 });
